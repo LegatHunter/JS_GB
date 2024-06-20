348ccadb7019412b08f4c576fe26af4e92717099
Nikita Prebyshevskiy <legathunter@mail.ru>
2024-06-20 21:31:59 +0300

    DZ_01 done!

---
+++ Seminar_01/script.js
@@ -0,0 +1,57 @@
+// № 1
+
+console.log("Текст из script.js");
+
+
+
+// № 2
+
+// num  = 123
+const num = 123;
+console.log(num);
+let a = 5;
+a = 7;
+/*a = 5
+a = 7*/
+console.log(a);
+let b = 1 + 2 + 3;
+console.log(b);
+
+
+
+// № 3
+
+const c = 10;
+const d = 2;
+let sum = c + d;
+let difference = c - d;
+let multiplication = c * d;
+let division = c / d;
+console.log(sum, difference, multiplication, division);
+// console.log(c + d+ sum + difference + multiplication + division);
+const result = c + d + sum + difference + multiplication + division;
+console.log(result);
+
+
+// № 5
+
+const a1 = 1.5;
+const b1 = 0.75;
+let res = a1 + b1;
+console.log(res);
+let number = 15;
+// console.log(number * (-1));
+console.log(-number);
+
+// № 6
+
+const fname = 'Никита'
+const lname = 'Пребышевский'
+console.log(fname, lname);
+const j = 'java'
+const s = 'script'
+console.log(j + s);
+const h = 'hello'
+const w = 'world'
+console.log(`${h} ${w}`);
+// console.log(h + ' ' + w);
