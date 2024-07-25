220c2175e2ee5a85225ac376a477459c397ae3f1
Nikita Prebyshevskiy <legathunter@mail.ru>
2024-07-25 16:40:44 +0300

    DZ_11 done! Траблы с каталогом

--- DZ_11/script.js
+++ DZ_11/script.js
@@ -1,10 +1,86 @@
-// Урок 11. Семинар. Шаблонизация
-// Дан макет сайта https://www.figma.com/file/mnLY69cYE5cqWM5w6n5hXx/Seo-%26-Digital-Marketing-Landing-Page?node-id=190%3A1194&t=q4NMnXTnwyyTSGA6-0
+const menuActive = document.querySelector(".menu__burger");
+const burger = document.querySelector(".menu");
+const menuClose = document.querySelector(".x");
 
-// В блоке Featured Items небходимо реализовать шаблон товаров.
+let expanded1 = false;
+let expanded2 = false;
+let expanded3 = false;
 
-// Для этого необходимо создать json формат данных по всем товарам.
+function toggleMenu() {
+  menuActive.classList.toggle("hidden__menu");
+}
+burger.addEventListener("click", toggleMenu);
+menuClose.addEventListener("click", toggleMenu);
 
-// Из этого файла сформировать блок Featured Items.
+function showCheckboxes1() {
+  const checkboxes1 = document.querySelector(".chois__trending_checkboxes");
+  if (!expanded1) {
+    checkboxes1.style.display = "block";
+    expanded1 = true;
+  } else {
+    checkboxes1.style.display = "none";
+    expanded1 = false;
+  }
+}
 
-// Всю вёрстку остальных частей реализовывать не нужно, если у вас она была сделана на html/css можно использовать, заново создавать не требуется.
+function showCheckboxes2() {
+  const checkboxes2 = document.querySelector(".chois__size_checkboxes");
+  if (!expanded2) {
+    checkboxes2.style.display = "block";
+    expanded2 = true;
+  } else {
+    checkboxes2.style.display = "none";
+    expanded2 = false;
+  }
+}
+
+function showCheckboxes3() {
+  const checkboxes3 = document.querySelector(".chois__price_checkboxes");
+  if (!expanded3) {
+    checkboxes3.style.display = "block";
+    expanded3 = true;
+  } else {
+    checkboxes3.style.display = "none";
+    expanded3 = false;
+  }
+}
+
+const url = "./json.json";
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+  const data = await getData(url);
+  const list = document.querySelector(".grid__product");
+  data.forEach((e) => {
+    list.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="card${e.count} cards">
+                        <img src="${e.img}" alt="" />
+                        <div class="card${e.count}__text pad">
+                            <h6>${e.tittle}</h6>
+                            <p class="card__info">
+                                ${e.desc}
+                            </p>
+                            <p class="card__price">$${e.price}</p>
+                            <div class="card__hover">
+                                <div class="card__hover__button">
+                                    <button>
+                                        <img src="./img/Cart.svg" alt="" />Add
+                                        to&nbsp;Cart
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+    );
+  });
+});
